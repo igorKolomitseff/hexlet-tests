f050be7e43b331d71850be60e22681f78982a8fc
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2023-08-01 13:40:53 +0300

    Add the assert construct to test_capitalize.py

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,11 +1,4 @@
 from capitalize import capitalize
 
-
-if capitalize('hello') != 'Hello':
-    raise Exception('The function is not working correctly!')
-
-if capitalize('') != '':
-    raise Exception('The function is not working correctly!')
-
-
-print('All tests are passed!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
